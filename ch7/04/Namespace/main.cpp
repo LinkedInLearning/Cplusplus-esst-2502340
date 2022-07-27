d04f828d3ebe7e6bb843f28046c14390f0e8e5fd
Gerhard Stein <gerstrong@gmail.com>
2022-07-27 18:13:18 +0200

    Added more examples

---
+++ ch7/04/Namespace/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+namespace lil
+{
+    class vector
+    {
+    public:
+
+        vector(const std::initializer_list<int> list)
+        {
+            for(const auto &elem : list)
+            {
+                push_back(elem);
+            }
+        }
+
+        vector(const int val)
+        {
+            push_back(val);
+        }
+
+        vector() = default; // trivial constructor
+
+        vector(const vector &myCopy) // Special copy constructor
+        {
+            delete [] mData;
+
+            mSize = myCopy.mSize;
+            mData = new int [mSize];
+
+            for(int i=0 ; i<mSize ; i++) {
+                mData[i] = myCopy[i];
+            }
+        }
+
+        ~vector() { delete [] mData; }
+
+        void push_back(const int val)
+        {
+            int *tempData = new int[mSize+1];
+
+            for(unsigned int i=0 ; i<mSize ; i++)
+            {
+                tempData[i] = mData[i];
+            }
+
+            tempData[mSize] = val;
+            delete [] mData; // deleting nullptr does not make it crash
+            mData = tempData;
+            mSize++;
+        }
+
+        int operator[](const unsigned int idx)
+        {
+            return mData[idx];
+        }
+
+        int operator[](const unsigned int idx) const
+        {
+            return mData[idx];
+        }
+
+        int getSize()
+        {   return mSize;   }
+
+    private:
+        int *mData = nullptr;
+        int mSize = 0;
+    };
+};
+
+
+int main()
+{
+    const int val = 20;
+
+    lil::vector lilVec1 {16, 23, 34};
+    lilVec1.push_back(val);
+
+    lil::vector lilVec2 = lilVec1;
+
+    std::cout << "lilVec1: " << std::endl;
+    for(int i=0 ; i<lilVec1.getSize() ; i++)    {
+        std::cout << lilVec1[i] << std::endl;
+    }
+
+    std::cout << "lilVec2: " << std::endl;
+    for(int i=0 ; i<lilVec2.getSize() ; i++){
+        std::cout << lilVec2[i] << std::endl;
+    }
+
+    return 0;
+}
