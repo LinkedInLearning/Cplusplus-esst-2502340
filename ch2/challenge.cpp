a20b70ca8f62859ae5be79cfdfb9b8a21b87f5bf
Gerhard Stein <gerstrong@gmail.com>
2022-08-11 08:17:12 +0200

    Solution for Chapter 2 challenge added

--- ch2/challenge.cpp
+++ ch2/challenge.cpp
@@ -13,13 +13,50 @@ int main()
 {
     std::ifstream file("numbers.csv");
 
-	// TODO: Check wheter file can be opened
+    if(!file)
+    {
+        std::cerr << "Error reading csv-file!" << std::endl;
+        return 1;
+    }
 
-    // TODO: Read the matrix from file
+    // Read the matrix
+    for( unsigned int i=0 ; i<numRows ; i++ )
+    {
+        for( unsigned int j=0 ; j<numCols ; j++ )
+        {
+            auto &row = matrix[i];
+            file >> row[j];
+        }
+    }
 
-    // TODO: Calculate the sums
+    std::array<float, numCols> horSum;
+    std::array<float, numRows> vertSum;
+    horSum.fill(0.0f);
+    vertSum.fill(0.0f);
 
-    // TODO: Display the sums
+    // Calculate the sums
+    for( unsigned int i=0 ; i<numRows ; i++ )
+    {
+        for( unsigned int j=0 ; j<numCols ; j++ )
+        {
+            auto &row = matrix[i];
+            horSum[i] += row[j];
+            vertSum[j] += row[j];
+        }
+    }
+
+    // Display the sums
+    for( unsigned int i=0 ; i<horSum.size() ; i++ )
+    {
+        std::cout << "horSum[" << i << "]: " << horSum[i];
+        std::cout << std::endl;
+    }
+
+    for( unsigned int j=0 ; j<vertSum.size() ; j++ )
+    {
+        std::cout << "vertSum[" << j << "]: " << vertSum[j];
+        std::cout << std::endl;
+    }
 
     return 0;
 }
